243a1ecc5f0270731ead6ca4b41628671e323268
livelyfun <kirakeshav158@gmail.com>
2026-07-26 12:02:21 +0545

    feat: add central widget and display layout

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ import sys
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
+    QLabel,
+    QVBoxLayout,
+    QWidget,
 )
 
 app = QApplication(sys.argv)
@@ -10,7 +13,29 @@ window = QMainWindow()
 window.setWindowTitle("Kira Calculator")
 window.resize(400, 600)
 
+# -----------------------
+# Central Widget 
+# -----------------------
 
+central_widget = QWidget()
+
+window.setCentralWidget(central_widget)
+
+# ------------------------
+# Layout
+# ------------------------
+
+layout = QVBoxLayout()
+
+central_widget.setLayout(layout)
+
+# -------------------------
+# Display
+# -------------------------
+
+display = QLabel("0")
+
+layout.addWidget(display)
 window.show()
 
 app.exec()
