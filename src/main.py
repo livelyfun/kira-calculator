af64c39dd5f8286c710dacc936f3c38661a13168
livelyfun <kirakeshav158@gmail.com>
2026-07-27 15:14:34 +0545

    feat: add calculator kepad layout

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from PySide6.QtWidgets import (
     QLabel,
     QVBoxLayout,
     QWidget,
+    QPushButton,
+    QGridLayout,
 )
 
 app = QApplication(sys.argv)
@@ -36,6 +38,31 @@ central_widget.setLayout(layout)
 display = QLabel("0")
 
 layout.addWidget(display)
+
+# -------------------------
+# Button Grid
+# -------------------------
+
+grid = QGridLayout()
+layout.addLayout(grid)
+
+# <- Add the button here 
+
+buttons = [
+    "7", "8", "9", "+",
+     "4", "5", "6", "×",
+    "1", "2", "3", "-",
+    "C", "0", "=", "+",
+]
+
+for index, text in enumerate(buttons):
+    buttons = QPushButton(text)
+
+    row = index //4
+    column = index % 4
+
+    grid.addWidget(buttons, row, column)
+
 window.show()
 
 app.exec()
