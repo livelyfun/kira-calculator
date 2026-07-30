e020049d384445147a1f5cd83a276c33a6d3c564
livelyfun <kirakeshav158@gmail.com>
2026-07-30 16:50:02 +0545

    refactor: move application styles to QSS

--- src/main.py
+++ src/main.py
@@ -1,86 +1,112 @@
 import sys
+
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow,
+    QGridLayout,
     QLabel,
+    QMainWindow,
+    QPushButton,
+    QSizePolicy,
     QVBoxLayout,
     QWidget,
-    QPushButton,
-    QGridLayout,
 )
 
 app = QApplication(sys.argv)
 
+# Load stylesheet
 with open("src/styles/main.qss", "r") as file:
     app.setStyleSheet(file.read())
+
+# -----------------------
+# Main Window
+# -----------------------
 
 window = QMainWindow()
 window.setWindowTitle("Kira Calculator")
 window.resize(400, 600)
 
 # -----------------------
-# Central Widget 
+# Central Widget
 # -----------------------
 
 central_widget = QWidget()
-
 window.setCentralWidget(central_widget)
 
-# ------------------------
-# Layout
-# ------------------------
+# -----------------------
+# Main Layout
+# -----------------------
 
 layout = QVBoxLayout()
+layout.setSpacing(15)
+layout.setContentsMargins(15, 15, 15, 15)
 
 central_widget.setLayout(layout)
 
-# -------------------------
+# -----------------------
 # Display
-# -------------------------
+# -----------------------
 
 display = QLabel("0")
-display.setStyleSheet("""
-    font-size: 36px;
-    border: 2px solid gray;
-    padding: 10px
-""")
+
+display.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+display.setMinimumHeight(100)
+
+display.setSizePolicy(
+    QSizePolicy.Policy.Expanding,
+    QSizePolicy.Policy.Fixed,
+)
 
 layout.addWidget(display)
 
-# -------------------------
+# -----------------------
 # Button Grid
-# -------------------------
+# -----------------------
 
 grid = QGridLayout()
+grid.setSpacing(10)
+
 layout.addLayout(grid)
 
-# <- Add the button here 
+layout.setStretch(0, 0)
+layout.setStretch(1, 1)
 
 buttons = [
     "7", "8", "9", "÷",
-     "4", "5", "6", "×",
+    "4", "5", "6", "×",
     "1", "2", "3", "-",
     "C", "0", "=", "+",
 ]
+
 
 def button_clicked(text):
     current = display.text()
 
     if current == "0":
         display.setText(text)
-    
     else:
         display.setText(current + text)
 
+
 for index, text in enumerate(buttons):
+
     button = QPushButton(text)
 
-    button.clicked.connect(lambda checked=False, t=text:  button_clicked(t))
-    
-    row = index //4
+    button.clicked.connect(
+        lambda checked=False, t=text: button_clicked(t)
+    )
+
+    row = index // 4
     column = index % 4
 
     grid.addWidget(button, row, column)
+
+for row in range(4):
+    grid.setRowStretch(row, 1)
+
+for column in range(4):
+    grid.setColumnStretch(column, 1)
 
 window.show()
 
